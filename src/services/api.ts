ff7db499e5f5407b37fafaaa62939b06cf18b6c8
Amrit Amber <bt22csa015@iiitn.ac.in>
2025-05-01 02:49:20 +0530

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ if (import.meta.env.MODE === 'development') {
   API_URL = 'http://localhost:5000';
 } else {
   // Use your EC2 public IP
-  API_URL = 'http://52.66.179.66';
+  API_URL = 'https://xlhsauhi76.execute-api.ap-south-1.amazonaws.com/v1';
 }
 
 export default API_URL;
